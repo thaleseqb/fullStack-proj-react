93ec17c218ff6ff43b053205322ada92c3d5986c
thaleseqb <thalesquadros2@gmail.com>
2024-07-12 16:49:20 -0300

    create a search bar

---
+++ src/Components/Search/data.js
@@ -0,0 +1,8 @@
+import book from "../../assets/livro.png"
+
+export const books = [
+    {nome: 'Liderança em Design', id: 1, src: book},
+    {nome: 'JavaScript Assertivo', id: 2, src:book},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src:book},
+    {nome: 'Aventureiros do Java', id: 4, src:book},
+]
--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,18 +1,95 @@
+import { useState } from "react";
+import { books } from "./data";
 import styled from "styled-components";
 
 const Input = styled.input`
-    border-radius: 4rem;
-    border: none;
+    order: 1px solid #FFF;
+    background: transparent;
+    border: 1px solid #FFF;
+    padding: 20px 140px;
+    border-radius: 50px;
+    width: 200px;
+    color: #FFF;
+    font-size: 16px;
+    margin-bottom: 10px;
+
+    &::placeholder {
+        color: #FFF;
+        font-size: 16px;
+    }
+`
+
+const SectionContainer = styled.section`
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
+`
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+`
+const SubTitle = styled.h3`
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
+
+const SearchResult = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
 `
 
 const Search = ({placeholder, type}) => {
 
+    const [searchedBooks, setSearchedBooks] = useState([]);
+
     return (
-        <section>
-            <h2>Já sabe por onde começar ?</h2>
-            <h3>Encontre seu livro em nossa estante</h3>
-            <Input placeholder={placeholder} type={type} />
-        </section>
+        <SectionContainer>
+            <Title>Já sabe por onde começar ?</Title>
+            <SubTitle>Encontre seu livro em nossa estante</SubTitle>
+            <Input 
+                placeholder={placeholder} 
+                type={type}
+                onBlur={(event) => {
+                    const wroteText = event.target.value;
+
+                    const searchResult = books.filter(book => {
+                        return book.
+                            nome.
+                            toLocaleLowerCase().
+                            includes(wroteText.toLocaleLowerCase())  
+                    });
+                    setSearchedBooks(searchResult);
+                }}
+            />
+
+            {searchedBooks.map(book => {
+                return (
+                    <SearchResult key={book.nome} >
+                        <img src={book.src} alt={`livro cujo id é ${book.id}`} />
+                        <p>{book.nome}</p>
+                    </SearchResult>
+                )
+            })}
+            
+        </SectionContainer>
     )
 }
 
